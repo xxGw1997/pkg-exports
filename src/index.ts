276045fd75bf20d2e00d431ef34d85e1b4748dc9
xxgw <wenxuan.yang@dragonsoftbravo.com>
2022-11-16 15:26:49 +0800

    test

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
-export async function getExports(name: string) {
-    const pkg = await import(name)
-    return Object.keys(pkg)
+import { resolve as resolvePackagePath } from 'mlly'
+
+export interface GetExportsOptions {
+  url?: string
+}
+
+export async function getExports(name: string, options?: GetExportsOptions) {
+  const path = await resolvePackagePath(name, { url: options?.url })
+  const pkg = await import(path)
+  const keys = Object.keys(pkg)
+  if (keys.length === 1 && keys[0] === 'default')
+    return Object.keys(pkg.default)
+  return keys
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,16 +1,49 @@
+import { resolve as resolvePackagePath } from 'mlly'
 import { describe, expect, it } from 'vitest'
 import { getExports } from '../src'
 
-describe('should', () => {
-  it('exported', async () => {
-    expect(await getExports('@antfu/utils')).toMatchInlineSnapshot(`
-      [
-        "assert",
-        "at",
-        "batchInvoke",
-        "clamp",
-        "clampArrayRange",
-      ]
-    `)
+describe('ESM', () => {
+  it('@antfu/utils', async () => {
+    expect((await getExports('@antfu/utils')).slice(0, 5))
+      .toMatchInlineSnapshot(`
+        [
+          "assert",
+          "at",
+          "batchInvoke",
+          "clamp",
+          "clampArrayRange",
+        ]
+      `)
+  })
+
+  it('@vue/shared', async () => {
+    expect((await getExports('@vue/shared', {
+      url: await resolvePackagePath('vue'),
+    })).slice(6, 12))
+      .toMatchInlineSnapshot(`
+        [
+          "camelize",
+          "capitalize",
+          "def",
+          "default",
+          "escapeHtml",
+          "escapeHtmlComment",
+        ]
+      `)
+  })
+})
+
+describe('CJS', () => {
+  it('axios', async () => {
+    expect((await getExports('axios')).slice(0, 5))
+      .toMatchInlineSnapshot(`
+        [
+          "request",
+          "getUri",
+          "delete",
+          "get",
+          "head",
+        ]
+      `)
   })
 })
